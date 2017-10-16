c4ddc02d642e5da475290e2713e17f4c62aa3da6
Pablo Pesce <papesce@gmail.com>
2017-10-16 00:03:59 -0300

    fix in pod

--- BalancedGym/RestApiManager.swift
+++ BalancedGym/RestApiManager.swift
@@ -15,19 +15,19 @@ class RestApiManager {
     let baseURL = "https://balanced-gym-api.herokuapp.com/"
     
     func getAllRoutines() {
-        Alamofire.request(baseURL).responseJSON { response in
-            print(response.request)  // original URL request
-            print(response.response) // HTTP URL response
-            print(response.data)     // server data
-            print(response.result)   // result of response serialization
-            
+        //Alamofire.request(baseURL).responseJSON { response in
+        //    print(response.request)  // original URL request
+        //    print(response.response) // HTTP URL response
+       //     print(response.data)     // server data
+       //     print(response.result)   // result of response serialization
+       
             //Optional binding to handle exceptions
            // if let json = response.result.value as? [[String:String]] {
            //     self.newsData = json.map{ News(dictionary: $0) }
            //
            //
            // }
-        }
+       // }
     }
     
    
