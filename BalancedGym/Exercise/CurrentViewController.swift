45f0cbeeb135d15f5b8bb3c78ade704ceb17fd74
Pablo Pesce <papesce@gmail.com>
2017-10-18 01:51:04 -0300

    Add InputText and numeric keyboard

---
+++ BalancedGym/Exercise/CurrentViewController.swift
@@ -0,0 +1,103 @@
+//
+//  CurrentViewController.swift
+//  BalancedGym
+//
+//  Created by Pablou on 10/17/17.
+//  Copyright © 2017 Pablou. All rights reserved.
+//
+
+import UIKit
+
+class CurrentViewController: UIViewController, UITextFieldDelegate {
+
+    var exercise: Exercise = Exercise(name: "default")
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var editableField: UITextField!
+    
+//    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        // limit to 4 characters
+//        let characterCountLimit = 4
+//
+//        // We need to figure out how many characters would be in the string after the change happens
+//        let startingLength = textField.text?.characters.count ?? 0
+//        let lengthToAdd = string.characters.count
+//        let lengthToReplace = range.length
+//
+//        let newLength = startingLength + lengthToAdd - lengthToReplace
+//
+//        return newLength <= characterCountLimit
+//    }
+    
+    
+    
+    //@IBAction func userTappedBackground(sender: AnyObject) {
+    //    view.endEditing(true)
+   // }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        self.titleLabel.text = editableField.text
+    }
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func addAccessoryView() -> Void {
+        //init toolbar
+        let toolbar:UIToolbar = UIToolbar(frame: CGRect(x: 0, y: 0,  width: self.view.frame.size.width, height: 30))
+        //create left side empty space so that done button set on right side
+        let flexSpace = UIBarButtonItem(barButtonSystemItem:    .flexibleSpace, target: nil, action: nil)
+        let doneBtn: UIBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(CurrentViewController.doneButtonAction))
+        toolbar.setItems([flexSpace, doneBtn], animated: false)
+        toolbar.sizeToFit()
+        //setting toolbar as inputAccessoryView
+        self.editableField.inputAccessoryView = toolbar
+    }
+ 
+    @objc func doneButtonAction() {
+        self.view.endEditing(true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.titleLabel.text = self.exercise.name
+        //self.navigationItem.title = "demo";
+        // Do any additional setup after loading the view.
+        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
+        
+        //init toolbar
+        self.addAccessoryView()
+        
+    }
+    
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        if segue.identifier == "currentSegueID" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let vc = segue.destination as! ExerciseTableViewController
+                let selectedRoutine = routines[indexPath.row]
+                vc.routine = selectedRoutine
+            }
+        }
+     */
+
+}
--- BalancedGym/NavigationTables/RoutineTableViewController.swift
+++ BalancedGym/NavigationTables/RoutineTableViewController.swift
@@ -1,20 +1,35 @@
 //
-//  DetailsTableViewController.swift
+//  RoutineTableViewController.swift
 //  BalancedGym
 //
-//  Created by Pablou on 10/10/17.
+//  Created by Pablou on 10/6/17.
 //  Copyright © 2017 Pablou. All rights reserved.
 //
 
 import UIKit
+import Alamofire
 
-class ExerciseTableViewController: UITableViewController {
+class RoutineTableViewController: UITableViewController {
 
-    var routine = Routine(name: "Default", exercises: [])
+    //MARK: Properties
+    var routines = [Routine]();
+    
+    
+    private func downloadData() {
+        RestApiManager.sharedInstance.executeRequest(completionHandler: { routines in
+            self.routines = routines
+            self.tableView.reloadData()
+        })
+        
+            
+        
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = routine.name
+        //Load sample data
+        // loadSampleRoutines()
+        downloadData()
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -32,27 +47,28 @@ class ExerciseTableViewController: UITableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return routine.exercises.count
+        return routines.count
     }
+
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Table view cells are reused and should be dequeued using a cell identifier.
-        let cellIdentifier = "ExerciseTableViewCell"
+        let cellIdentifier = "RoutineTableViewCell"
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier,
-               for: indexPath) as? ExerciseTableViewCell else {
-           fatalError("The dequeued cell is not an instance of ExerciseTableViewCell.")
+         for: indexPath) as? RoutineTableViewCell else {
+            fatalError("The dequeued cell is not an instance of MealTableViewCell.")
         }
-        let exercise = routine.exercises[indexPath.row]
+        let routine = routines[indexPath.row]
         // Configure the cell
-        cell.nameLabel.text = exercise.name;
+        cell.nameLabel.text = routine.name;
         //cell.photoImageView.image = routine.photo;
-        
+
         return cell
     }
-  
-  
+    
+
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
@@ -88,14 +104,21 @@ class ExerciseTableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "mySegue"{
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let vc = segue.destination as! ExerciseTableViewController
+                let selectedRoutine = routines[indexPath.row]
+                vc.routine = selectedRoutine
+            }
+        }
     }
-    */
+    
 
 }
